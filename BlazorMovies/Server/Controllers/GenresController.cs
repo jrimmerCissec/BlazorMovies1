feaab18c0bd016a513c32fd05e130a5198e2db93
Justin Rimmer <justinrimmer@gmail.com>
2020-10-06 22:14:57 -0400

    Initial Upload

---
+++ BlazorMovies/Server/Controllers/GenresController.cs
@@ -0,0 +1,31 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+
+        public GenresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Genre genre)
+        {
+            context.Add(genre);
+            await context.SaveChangesAsync();
+            return genre.Id;
+        }
+    }
+}
---
+++ BlazorMovies/Server/Controllers/PeopleController.cs
@@ -0,0 +1,41 @@
+﻿using BlazorMovies.Server.Helpers;
+using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Server.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IFileStorageService fileStorageService; 
+
+
+        public PeopleController(ApplicationDbContext context, IFileStorageService fileStorageService)
+        {
+            this.context = context;
+            this.fileStorageService = fileStorageService;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Person person)
+        {
+            if(!string.IsNullOrEmpty(person.Picture))
+            {
+                var personPicture = Convert.FromBase64String(person.Picture);
+                person.Picture = await fileStorageService.SaveFile(personPicture, "jpg", "people");
+            }
+
+            context.Add(person);
+            await context.SaveChangesAsync();
+            return person.Id;
+        }
+    }
+}
